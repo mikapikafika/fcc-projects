feedac6cb8fa16f1351da5ce906b992eedab1011
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-16 13:40:52 +0200

    Quotes API works

--- FrontEnd-Development-Libraries/random-quote-machine/script.js
+++ FrontEnd-Development-Libraries/random-quote-machine/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
+    // DOM variables
+    const quote = $("#text");
+    const author =$("#author");
+    const button = $("#new-quote");
 
     async function newQuote() {
         const response = await fetch("https://api.quotable.io/random");
         const data = await response.json();
+        console.log(data);
         if (!response.ok) {
-
+            quote.textContent = "Oops... something went wrong.";
         } else {
-
+            quote.text(data.content);
+            author.text(data.author);
         }
     }
 
+    button.click(newQuote);
+    // To initially show a quote when the page loads
+    newQuote()
 });
