0dd985a27df3ba50ef6d21adc199dc8413f00bb0
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-17 17:53:40 +0200

    Finished Markdown Previewer

--- FrontEnd-Development-Libraries/markdown-previewer/src/App.js
+++ FrontEnd-Development-Libraries/markdown-previewer/src/App.js
@@ -3,8 +3,33 @@ import Editor from "./Editor";
 import Preview from "./Preview";
 import {useState} from "react";
 
+const initialMarkdown = `
+# Title Header (H1 Header)
+## Sub Header (H2 header)
+
+[Link to a hacker typer website to feel like a hacker](https://hackertyper.com)
+
+Here's some \`inline code\`
+
+\`\`\`
+// And here's a block of code:
+function example() {
+  return "Hello, World!";
+}
+\`\`\`
+
+- This is a list item
+- This list is unordered
+
+> Imagine a profound quote here
+
+![Image](https://64.media.tumblr.com/25ec1da1ceb3d8c59ff61abda466e66d/tumblr_mq5iy5uA1K1s5jjtzo1_500.gif)
+
+**This text is bold**
+`;
+
 function App() {
-    const [markdownText, setMarkdownText] = useState("");
+    const [markdownText, setMarkdownText] = useState(initialMarkdown);
 
     return (
         <div className="App">
--- FrontEnd-Development-Libraries/markdown-previewer/src/Preview.js
+++ FrontEnd-Development-Libraries/markdown-previewer/src/Preview.js
@@ -1,33 +1,8 @@
 import {marked} from "marked";
 import './EditorPreview.scss';
 
-const initialMarkdown = `
-# Title Header (H1 Header)
-## Sub Header (H2 header)
-
-[Link to a hacker typer website to feel like a hacker](https://hackertyper.com)
-
-Here's some \`inline code\`
-
-\`\`\`
-// And here's a block of code:
-function example() {
-  return "Hello, World!";
-}
-\`\`\`
-
-- This is a list item
-- This list is unordered
-
-> Imagine a profound quote here
-
-![Image](https://64.media.tumblr.com/25ec1da1ceb3d8c59ff61abda466e66d/tumblr_mq5iy5uA1K1s5jjtzo1_500.gif)
-
-**This text is bold**
-`;
-
 function Preview({markdownText}) {
-    const convertedText = marked(markdownText || initialMarkdown);
+    const convertedText = marked(markdownText);
 
     return (
         <div className="Preview">
