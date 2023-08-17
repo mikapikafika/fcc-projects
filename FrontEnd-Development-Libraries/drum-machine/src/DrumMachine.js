c9e40b871399c7c96c36ba1d9325220ece8189a8
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-18 00:36:11 +0200

    Added different sounds

--- FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
+++ FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
@@ -1,41 +1,47 @@
 import {useEffect, useState} from "react";
 import "./DrumMachine.scss";
-import audio1 from "./audio/Cev_H2.mp3";
-import audio2 from "./audio/Dsc_Oh.mp3";
-import audio3 from "./audio/Heater-1.mp3";
-import audio4 from "./audio/Heater-2.mp3";
-import audio5 from "./audio/Heater-3.mp3";
-import audio6 from "./audio/Heater-4_1.mp3";
-import audio7 from "./audio/Heater-6.mp3";
-import audio8 from "./audio/Kick_n_Hat.mp3";
+import audio1 from "./audio/slip.mp3";
+import audio2 from "./audio/dash.mp3";
+import audio3 from "./audio/rubber.mp3";
+import audio4 from "./audio/bite.mp3";
+import audio5 from "./audio/stealth.mp3";
+import audio6 from "./audio/running.mp3";
+import audio7 from "./audio/dash2.mp3";
+import audio8 from "./audio/trumpet.mp3";
 import audio9 from "./audio/RP4_KICK_1.mp3";
 
 const drumPads = [
-    {id: "Q", keyCode: 81, audioClip: audio1, description: "Closed HH"},
-    {id: "W", keyCode: 87, audioClip: audio2, description: "Open HH"},
-    {id: "E", keyCode: 69, audioClip: audio3, description: "Heater 1"},
-    {id: "A", keyCode: 65, audioClip: audio4, description: "Heater 2"},
-    {id: "S", keyCode: 83, audioClip: audio5, description: "Heater 3"},
-    {id: "D", keyCode: 68, audioClip: audio6, description: "Heater 4"},
-    {id: "Z", keyCode: 90, audioClip: audio7, description: "Clap"},
-    {id: "X", keyCode: 88, audioClip: audio8, description: "Kick n' Hat"},
+    {id: "Q", keyCode: 81, audioClip: audio1, description: "Slip"},
+    {id: "W", keyCode: 87, audioClip: audio2, description: "Dash"},
+    {id: "E", keyCode: 69, audioClip: audio3, description: "Rubber"},
+    {id: "A", keyCode: 65, audioClip: audio4, description: "Bite"},
+    {id: "S", keyCode: 83, audioClip: audio5, description: "Stealth"},
+    {id: "D", keyCode: 68, audioClip: audio6, description: "Running"},
+    {id: "Z", keyCode: 90, audioClip: audio7, description: "Dash 2"},
+    {id: "X", keyCode: 88, audioClip: audio8, description: "Trumpet"},
     {id: "C", keyCode: 67, audioClip: audio9, description: "Kick"},
 ];
 
 function DrumMachine() {
     const [displayedText, setDisplayedText] = useState("");
+    const [activePad, setActivePad] = useState(null);
 
     // Key pressing and clicking
 
     const handleKeyPress = (event) => {
-        event.preventDefault(); // Prevent default key behavior
+        event.preventDefault();
         const keyPressed = event.key.toUpperCase();
         const drumPad = drumPads.find((pad) => pad.id === keyPressed);
         if (drumPad) {
             const audio = new Audio(drumPad.audioClip);
             audio.play();
             setDisplayedText(drumPad.description);
+            setActivePad(keyPressed);
         }
+    };
+
+    const handleKeyUp = () => {
+        setActivePad(null);
     };
 
     useEffect(() => {
@@ -44,8 +50,11 @@ function DrumMachine() {
         };
 
         window.addEventListener("keydown", handleKeyDown);
+        window.addEventListener("keyup", handleKeyUp);
+
         return () => {
             window.removeEventListener("keydown", handleKeyDown);
+            window.removeEventListener("keyup", handleKeyUp);
         };
     }, []);
 
