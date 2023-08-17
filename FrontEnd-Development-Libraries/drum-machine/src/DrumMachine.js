9ab4bcb5c9b060224f9c27e1734d5889b43d9f28
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-17 21:18:23 +0200

    Added some styling for the drum pads

--- FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
+++ FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
@@ -1,4 +1,5 @@
 import {useEffect, useState} from "react";
+import "./DrumMachine.scss";
 import audio1 from "./audio/Cev_H2.mp3";
 import audio2 from "./audio/Dsc_Oh.mp3";
 import audio3 from "./audio/Heater-1.mp3";
@@ -10,23 +11,26 @@ import audio8 from "./audio/Kick_n_Hat.mp3";
 import audio9 from "./audio/RP4_KICK_1.mp3";
 
 const drumPads = [
-    {id: "Q", keyCode: 81, audioClip: audio1, description: "Closed HH" },
-    {id: "W", keyCode: 87, audioClip: audio2, description: "Open HH" },
-    {id: "E", keyCode: 69, audioClip: audio3, description: "Heater 1" },
-    {id: "A", keyCode: 65, audioClip: audio4, description: "Heater 2" },
-    {id: "S", keyCode: 83, audioClip: audio5, description: "Heater 3" },
-    {id: "D", keyCode: 68, audioClip: audio6, description: "Heater 4" },
-    {id: "Z", keyCode: 90, audioClip: audio7, description: "Clap" },
-    {id: "X", keyCode: 88, audioClip: audio8, description: "Kick n' Hat" },
-    {id: "C", keyCode: 67, audioClip: audio9, description: "Kick" },
+    {id: "Q", keyCode: 81, audioClip: audio1, description: "Closed HH"},
+    {id: "W", keyCode: 87, audioClip: audio2, description: "Open HH"},
+    {id: "E", keyCode: 69, audioClip: audio3, description: "Heater 1"},
+    {id: "A", keyCode: 65, audioClip: audio4, description: "Heater 2"},
+    {id: "S", keyCode: 83, audioClip: audio5, description: "Heater 3"},
+    {id: "D", keyCode: 68, audioClip: audio6, description: "Heater 4"},
+    {id: "Z", keyCode: 90, audioClip: audio7, description: "Clap"},
+    {id: "X", keyCode: 88, audioClip: audio8, description: "Kick n' Hat"},
+    {id: "C", keyCode: 67, audioClip: audio9, description: "Kick"},
 ];
 
 function DrumMachine() {
     const [displayedText, setDisplayedText] = useState("");
 
+    // Key pressing and clicking
+
     const handleKeyPress = (event) => {
+        event.preventDefault(); // Prevent default key behavior
         const keyPressed = event.key.toUpperCase();
-        const drumPad = drumPads.find( (pad) => pad.id === keyPressed);
+        const drumPad = drumPads.find((pad) => pad.id === keyPressed);
         if (drumPad) {
             const audio = new Audio(drumPad.audioClip);
             audio.play();
@@ -35,9 +39,13 @@ function DrumMachine() {
     };
 
     useEffect(() => {
-        window.addEventListener("keydown", handleKeyPress);
+        const handleKeyDown = (event) => {
+            handleKeyPress(event);
+        };
+
+        window.addEventListener("keydown", handleKeyDown);
         return () => {
-            window.removeEventListener("keydown", handleKeyPress);
+            window.removeEventListener("keydown", handleKeyDown);
         };
     }, []);
 
@@ -52,24 +60,26 @@ function DrumMachine() {
             id="drum-machine"
             className="DrumMachine"
         >
-            {drumPads.map((pad) => (
-                    <div
-                        key={pad.id}
-                        className="drum-pad"
-                        id={pad.id}
-                        onClick={() => handlePadClick(pad.audioClip)}
-                    >
-                        {pad.id}
-                        <audio
-                            className="clip"
+            <div class="row">
+                {drumPads.map((pad) => (
+                        <div
+                            key={pad.id}
+                            className="drum-pad col text-center"
                             id={pad.id}
-                            src={pad.audioClip}
-                        />
-                    </div>
-                )
-            )}
-            <div id="display" className="Display">
-                {displayedText}
+                            onClick={() => handlePadClick(pad.audioClip, pad.description)}
+                        >
+                            {pad.id}
+                            <audio
+                                className="clip"
+                                id={pad.id}
+                                src={pad.audioClip}
+                            />
+                        </div>
+                    )
+                )}
+                <div id="display" className="Display">
+                    {displayedText}
+                </div>
             </div>
         </div>
     );
