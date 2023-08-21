0217e2f14af950cc256e93188fcd4ec3e4fd1b49
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-21 03:49:58 +0200

    Finished the Drum Machine project

--- FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
+++ FrontEnd-Development-Libraries/drum-machine/src/DrumMachine.js
@@ -8,7 +8,7 @@ import audio5 from "./audio/stealth.mp3";
 import audio6 from "./audio/running.mp3";
 import audio7 from "./audio/dash2.mp3";
 import audio8 from "./audio/trumpet.mp3";
-import audio9 from "./audio/RP4_KICK_1.mp3";
+import audio9 from "./audio/boink.mp3";
 
 const drumPads = [
     {id: "Q", keyCode: 81, audioClip: audio1, description: "Slip"},
@@ -19,35 +19,41 @@ const drumPads = [
     {id: "D", keyCode: 68, audioClip: audio6, description: "Running"},
     {id: "Z", keyCode: 90, audioClip: audio7, description: "Dash 2"},
     {id: "X", keyCode: 88, audioClip: audio8, description: "Trumpet"},
-    {id: "C", keyCode: 67, audioClip: audio9, description: "Kick"},
+    {id: "C", keyCode: 67, audioClip: audio9, description: "Boink"},
 ];
 
 function DrumMachine() {
     const [displayedText, setDisplayedText] = useState("");
     const [activePad, setActivePad] = useState(null);
-    const [volume, setVolume] = useState(0.5);
-
-    // Key pressing and clicking
 
     const handleKeyPress = (event) => {
         event.preventDefault();
+        // Retrieving the pressed key and finding a drumPad with such ID
         const keyPressed = event.key.toUpperCase();
         const drumPad = drumPads.find((pad) => pad.id === keyPressed);
 
         if (drumPad) {
+            // A new Audio object with associated audioClip
             const audio = new Audio(drumPad.audioClip);
             audio.play();
             setDisplayedText(drumPad.description);
-            setActivePad(keyPressed);
-            console.log(drumPad.id);
+            setActivePad(keyPressed);   //  needed for CSS reasons
         }
     };
 
+    const handlePadClick = (audioClip, description) => {
+        const audio = new Audio(audioClip);
+        audio.play();
+        setDisplayedText(description);
+    };
+
+    // A side effect to appear after the component renders
     useEffect(() => {
         const handleKeyDown = (event) => {
             handleKeyPress(event);
         };
 
+        // When a key is released
         const handleKeyUp = () => {
             setActivePad(null);
         };
@@ -59,13 +65,7 @@ function DrumMachine() {
             window.removeEventListener("keydown", handleKeyDown);
             window.removeEventListener("keyup", handleKeyUp);
         };
-    }, []);
-
-    const handlePadClick = (audioClip, description) => {
-        const audio = new Audio(audioClip);
-        audio.play();
-        setDisplayedText(description);
-    };
+    }, []); // should run once, not each time the component is rendered
 
     return (
         <div
@@ -90,7 +90,7 @@ function DrumMachine() {
                     )
                 )}
                 <div id="display" className="Display text-center mt-4">
-                    {displayedText || "Click/press me"}
+                    {displayedText || "Click/press a letter"}
                 </div>
             </div>
         </div>
