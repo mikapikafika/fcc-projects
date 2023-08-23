f384382cf5bcfa3b61b5f0e49d77d74211ba1833
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-23 13:33:33 +0200

    Added backspace and mapping over buttons

--- FrontEnd-Development-Libraries/javascript-calculator/src/Calculator.js
+++ FrontEnd-Development-Libraries/javascript-calculator/src/Calculator.js
@@ -3,6 +3,14 @@ import {useDispatch, useSelector} from "react-redux";
 import {setDisplayedValue, setEquation} from "./redux/actions";
 import {evaluate} from "mathjs";
 
+const buttons = [
+  ["C", "/", "*"],
+  ["7", "8", "9", "-"],
+  ["4", "5", "6", "+"],
+  ["1", "2", "3", "="],
+  ["0", ".", "←"]
+];
+
 function Calculator() {
     const displayedValue = useSelector((state) => state.displayedValue);
     const equation = useSelector((state) => state.equation);
@@ -18,11 +26,13 @@ function Calculator() {
 
         else if (buttonId === "=") {
             try {
-                const result = evaluate(equation);
+                // Performing calculations and displaying the result
+                const result = evaluate(equation);      // calculating
                 dispatch(setDisplayedValue(result.toString()));
                 dispatch(setEquation(result.toString()));
             } catch (error) {
-                dispatch(setDisplayedValue("Error"));
+                // If divided by 0 or invalid equation
+                dispatch(setDisplayedValue("Err"));
             }
         }
 
@@ -34,9 +44,22 @@ function Calculator() {
             }
         }
 
+        else if (buttonId === "←") {
+            if (displayedValue.length > 1) {
+                const newValue = displayedValue.slice(0, -1);
+                dispatch(setDisplayedValue(newValue));
+                dispatch(setEquation(equation.slice(0, -1)));
+            }
+
+            else if (displayedValue.length === 1) {
+                dispatch(setDisplayedValue("0"));
+                dispatch(setEquation(""));
+            }
+        }
+
 
         else {
-            if (displayedValue === '0' || displayedValue === 'Error') {
+            if (displayedValue === "0" || displayedValue === "Err") {
                 // Preventing multiple 0s
                 if (buttonId !== "0") {
                     dispatch(setDisplayedValue(buttonId));
@@ -58,23 +81,15 @@ function Calculator() {
 
     return (
         <div className="Calculator">
-            <button id="equals" onClick={() => handleButtonClick("=")}>=</button>
-            <button id="zero" onClick={() => handleButtonClick("0")}>0</button>
-            <button id="one" onClick={() => handleButtonClick("1")}>1</button>
-            <button id="two" onClick={() => handleButtonClick("2")}>2</button>
-            <button id="three" onClick={() => handleButtonClick("3")}>3</button>
-            <button id="four" onClick={() => handleButtonClick("4")}>4</button>
-            <button id="five" onClick={() => handleButtonClick("5")}>5</button>
-            <button id="six" onClick={() => handleButtonClick("6")}>6</button>
-            <button id="seven" onClick={() => handleButtonClick("7")}>7</button>
-            <button id="eight" onClick={() => handleButtonClick("8")}>8</button>
-            <button id="nine" onClick={() => handleButtonClick("9")}>9</button>
-            <button id="add" onClick={() => handleButtonClick("+")}>+</button>
-            <button id="substract" onClick={() => handleButtonClick("-")}>-</button>
-            <button id="multiply" onClick={() => handleButtonClick("*")}>*</button>
-            <button id="divide" onClick={() => handleButtonClick("/")}>/</button>
-            <button id="decimal" onClick={() => handleButtonClick(".")}>.</button>
-            <button id="clear" onClick={() => handleButtonClick("C")}>C</button>
+            {buttons.map( (row, index) => (
+                <div key={index} className="btn-row">
+                    {row.map( (buttonId) => (
+                        <button key={buttonId} id={buttonId} onClick={() => handleButtonClick(buttonId)}>
+                            {buttonId}
+                        </button>
+                    ))}
+                </div>
+            ))}
             <div id="display">{displayedValue}</div>
         </div>
     );
