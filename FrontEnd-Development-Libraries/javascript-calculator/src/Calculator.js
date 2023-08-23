5df43e074cbdc7a209fc9c1a00ba66247837ba62
230043 Michalina Wysocka <230043@edu.p.lodz.pl>
2023-08-23 12:27:15 +0200

    Before using Redux

--- FrontEnd-Development-Libraries/javascript-calculator/src/Calculator.js
+++ FrontEnd-Development-Libraries/javascript-calculator/src/Calculator.js
@@ -25,23 +25,23 @@ function Calculator() {
 
     return (
         <div className="Calculator">
-            <button id="equals" onClick={() => handleButtonClick('=')}>=</button>
-            <button id="zero" onClick={() => handleButtonClick('0')}>0</button>
-            <button id="one" onClick={() => handleButtonClick('1')}>1</button>
-            <button id="two">2</button>
-            <button id="three">3</button>
-            <button id="four">4</button>
-            <button id="five">5</button>
-            <button id="six">6</button>
-            <button id="seven">7</button>
-            <button id="eight">8</button>
-            <button id="nine">9</button>
-            <button id="add">+</button>
-            <button id="substract">-</button>
-            <button id="multiply">*</button>
-            <button id="divide">/</button>
-            <button id="decimal">.</button>
-            <button id="clear">C</button>
+            <button id="equals" onClick={() => handleButtonClick("=")}>=</button>
+            <button id="zero" onClick={() => handleButtonClick("0")}>0</button>
+            <button id="one" onClick={() => handleButtonClick("1")}>1</button>
+            <button id="two" onClick={() => handleButtonClick("2")}>2</button>
+            <button id="three" onClick={() => handleButtonClick("3")}>3</button>
+            <button id="four" onClick={() => handleButtonClick("4")}>4</button>
+            <button id="five" onClick={() => handleButtonClick("5")}>5</button>
+            <button id="six" onClick={() => handleButtonClick("6")}>6</button>
+            <button id="seven" onClick={() => handleButtonClick("7")}>7</button>
+            <button id="eight" onClick={() => handleButtonClick("8")}>8</button>
+            <button id="nine" onClick={() => handleButtonClick("9")}>9</button>
+            <button id="add" onClick={() => handleButtonClick("+")}>+</button>
+            <button id="substract" onClick={() => handleButtonClick("-")}>-</button>
+            <button id="multiply" onClick={() => handleButtonClick("*")}>*</button>
+            <button id="divide" onClick={() => handleButtonClick("/")}>/</button>
+            <button id="decimal" onClick={() => handleButtonClick(".")}>.</button>
+            <button id="clear" onClick={() => handleButtonClick("C")}>C</button>
             <div id="display">{displayedValue}</div>
         </div>
     );
